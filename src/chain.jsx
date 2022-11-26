5bd74507fe1dc7e5eb22a8dcce45065e92d8abdf
seumsims <seumworldinbangladesh@gmail.com>
2022-11-26 08:44:05 +0600

    master codepen react with vite completed

---
+++ src/chain.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import Logics from './elements/Logics'
+import './a.css'
+
+ReactDOM.createRoot(document.getElementById('divStar')).render(
+  <React.StrictMode>
+    <Logics />
+  </React.StrictMode>
+)
---
+++ src/elements/Logics.jsx
@@ -0,0 +1,12 @@
+import { useState } from 'react'
+
+
+function Logics() {
+  
+
+  return (
+    "Salam"
+  )
+}
+
+export default Logics
